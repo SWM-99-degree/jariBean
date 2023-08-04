345b8493acde81fe68c055f1643adafc8b423398
psy-choi <chlrltjd5263@naver.com>
2023-08-04 15:09:07 +0900

    feat : <기성 #57> Profile Controller
    1. updateAlarmStatus 메소드 작성 = PUT /api/profiles/alarm
    2. getProfile 메소드 작성 = GET /api/profiles
    3. updateProfile 메소드 작성 = PUT /api/profiles

---
+++ src/main/java/com/example/jariBean/dto/profile/ProfileReqDto.java
@@ -0,0 +1,25 @@
+package com.example.jariBean.dto.profile;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+
+public class ProfileReqDto {
+    @Getter
+    @Setter
+    public static class ProfileUpdateReqDto {
+
+        @NotEmpty(message = "nickname은 필수입니다")
+        @Pattern(regexp = "^[a-zA-Z0-9]{2,20}$", message = "영문/숫자 2~20자 이내로 작성해주세요.")
+        private String nickname;
+
+        private String imageUrl;
+
+        @Pattern(regexp = ".{2,50}$", message = "50자 이내로 작성해주세요.")
+        private String description;
+    }
+
+
+}
