d19a1ad9da9a6f0324f57c691c7000699fe1ebf8
psy-choi <chlrltjd5263@naver.com>
2023-08-10 13:26:29 +0900

    feat : <기성 # 70> dockerfile 작성

--- src/test/java/com/example/jariBean/FCM/FCMTests.java
+++ src/test/java/com/example/jariBean/FCM/FCMTests.java
@@ -4,7 +4,11 @@ package com.example.jariBean.FCM;
 import com.example.jariBean.service.FCMNotificationService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.InputStream;
 
 @SpringBootTest
 public class FCMTests {
@@ -12,11 +16,14 @@ public class FCMTests {
     @Autowired
     FCMNotificationService fcmNotificationService;
 
-
+    @Value("${FIREBASE_CLASS_PATH}")
+    private String FIREBASE_CLASS_PATH;
     @Test
     public void FCMTest() throws Exception{
 
-        fcmNotificationService.sendNotificationByTokentest("eiMZvMU4TvCk4BNeUEHBoz:APA91bH7pZYQBC_aTV0RR4Y2uvrvN1-5_kTrhZWkpzp3G5r03r86JuqRh3fPThKlgVNTQb_kp7_wsnVfQVh9x0XbiZscqSR6XT62CZ28m1SkBsnKmN12rlSHlDnqnS-v-7Z1gFGXxAv3");
+
+
+        fcmNotificationService.sendNotificationByTokentest("eiMZvMU4TvCk4BNeUEHBoz:APA91bG6uf_mg9I70YslVe4E6nOvrP6pvFkZ8BVIF-8YDnfqYM0tLNQYtMG6pVFdaHCBWWwEbsRBZg5GJ4MHp6RBTgufDOrXovJYxz53xGPWTXpLAEbfTtTmTXV7dtKR8PDENqpOPF74");
 
     }
 }
