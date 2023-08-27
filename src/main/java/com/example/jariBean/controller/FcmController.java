f6054dd814587f5cd987fd3f6f8113cd7fa18fa8
isayaksh <isayaksh@gmail.com>
2023-08-27 20:26:46 +0900

    refactor: <김상현 #126> add @ApiResponse options for swagger ui

--- src/main/java/com/example/jariBean/controller/FcmController.java
+++ src/main/java/com/example/jariBean/controller/FcmController.java
@@ -4,6 +4,8 @@ import com.example.jariBean.config.auth.LoginUser;
 import com.example.jariBean.dto.ResponseDto;
 import com.example.jariBean.dto.fcm.FCMReqDto.FCMTokenReqDto;
 import com.example.jariBean.service.FcmService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -12,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,12 +22,16 @@ import static org.springframework.http.HttpStatus.OK;
 public class FcmController {
 
     private final FcmService fcmService;
-
+    @Operation(summary = "store fcm token", description = "api for store fcm token")
+    @ApiResponse(
+            responseCode = "201",
+            description = "예약 내역 저장 성공"
+    )
     @PutMapping("/token")
     public ResponseEntity storingFCMTokenInRedis(@AuthenticationPrincipal LoginUser loginUser,
                                                  @RequestBody FCMTokenReqDto fcmTokenReqDto) {
         fcmService.updateFirebaseToken(loginUser.getUser().getId(), fcmTokenReqDto);
-        return new ResponseEntity<>(new ResponseDto<>(1, "FCM 토큰 정보 갱신에 성공하였습니다.", null), OK);
+        return new ResponseEntity<>(new ResponseDto<>(1, "FCM 토큰 정보 갱신에 성공하였습니다.", null), CREATED);
     }
 
 }
--- src/main/java/com/example/jariBean/controller/UserController.java
+++ src/main/java/com/example/jariBean/controller/UserController.java
@@ -5,6 +5,9 @@ import com.example.jariBean.dto.ResponseDto;
 import com.example.jariBean.dto.user.UserResDto.UserInfoRespDto;
 import com.example.jariBean.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -23,6 +26,11 @@ public class UserController {
     private final UserService userService;
 
     @Operation(summary = "find user me", description = "api for find user me")
+    @ApiResponse(
+            responseCode = "200",
+            description = "사용자 조회 성공",
+            content = @Content(schema = @Schema(implementation = UserInfoRespDto.class))
+    )
     @GetMapping("/me")
     public ResponseEntity findProfile(@AuthenticationPrincipal LoginUser loginUser) {
         UserInfoRespDto userInfo = userService.findUserInfo(loginUser.getUser().getId());
@@ -30,10 +38,14 @@ public class UserController {
     }
 
     @Operation(summary = "register user", description = "api for register user")
+    @ApiResponse(
+            responseCode = "200",
+            description = "사용자 회원가입 성공",
+            content = @Content(schema = @Schema(implementation = UserInfoRespDto.class))
+    )
     @PutMapping("/register")
     public ResponseEntity register(@AuthenticationPrincipal LoginUser loginUser) {
         UserInfoRespDto userInfo = userService.register(loginUser.getUser().getId());
-
         return new ResponseEntity<>(new ResponseDto<>(1, "회원가입 성공", userInfo), OK);
     }
 
