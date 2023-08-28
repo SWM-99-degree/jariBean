bfb259adc1535ea49d6669b747a0d87cba0e506c
isayaksh <isayaksh@gmail.com>
2023-08-28 19:27:54 +0900

    feat: <김상현 #131> create Token Controller and Service

---
+++ src/main/java/com/example/jariBean/service/TokenService.java
@@ -0,0 +1,41 @@
+package com.example.jariBean.service;
+
+import com.example.jariBean.config.jwt.JwtProcess;
+import com.example.jariBean.dto.oauth.LoginResDto.LoginSuccessResDto;
+import com.example.jariBean.entity.Role;
+import com.example.jariBean.entity.Token;
+import com.example.jariBean.handler.ex.CustomApiException;
+import com.example.jariBean.repository.TokenRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import static com.example.jariBean.config.jwt.JwtProcess.TokenType.ACCESS;
+import static com.example.jariBean.config.jwt.JwtProcess.TokenType.REFRESH;
+
+@Service
+@RequiredArgsConstructor
+public class TokenService {
+
+    private final TokenRepository tokenRepository;
+    private final JwtProcess jwtProcess;
+
+    public LoginSuccessResDto renewJWT(String userId, Role role, String refreshJWT) {
+
+        // find token set
+        Token fToken = tokenRepository.findById(userId)
+                .orElseThrow(() -> new CustomApiException("userId에 해당하는 Token set이 존재하지 않습니다."));
+
+        // create JWT
+        String accessToken = jwtProcess.createJWT(userId, role.toString(), ACCESS);
+        String refreshToken = jwtProcess.createJWT(userId, role.toString(), REFRESH);
+
+        // renew JWT
+        fToken.renewJWT(accessToken, refreshToken);
+        tokenRepository.save(fToken);
+
+        return LoginSuccessResDto.builder()
+                .accessToken(accessToken)
+                .refreshToken(refreshToken)
+                .build();
+    }
+}
