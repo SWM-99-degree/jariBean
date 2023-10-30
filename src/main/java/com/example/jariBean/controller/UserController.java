71dc9cf51d301f2932781570151914e3bfd88df3
isayaksh <isayaksh@gmail.com>
2023-10-30 16:18:58 +0900

    modify api contorller

--- src/main/java/com/example/jariBean/controller/UserController.java
+++ src/main/java/com/example/jariBean/controller/UserController.java
@@ -2,9 +2,14 @@ package com.example.jariBean.controller;
 
 import com.example.jariBean.config.auth.LoginUser;
 import com.example.jariBean.dto.ResponseDto;
+import com.example.jariBean.dto.oauth.LoginCode;
+import com.example.jariBean.dto.oauth.LoginResDto;
 import com.example.jariBean.dto.user.UserResDto.UserInfoRespDto;
 import com.example.jariBean.service.S3Service;
 import com.example.jariBean.service.UserService;
+import com.example.jariBean.service.oauth.OAuthKakaoService;
+import com.example.jariBean.service.oauth.OAuthService;
+import com.example.jariBean.service.oauth.OAuthServiceFactory;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -15,6 +20,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.io.IOException;
 
 import static org.springframework.http.HttpStatus.OK;
@@ -24,8 +30,10 @@ import static org.springframework.http.HttpStatus.OK;
 @RequestMapping("/api/users")
 public class UserController {
 
+    private final OAuthServiceFactory authServiceFactory;
     private final UserService userService;
     private final S3Service s3Service;
+    private OAuthService oAuthService;
 
     @Operation(summary = "find user me", description = "api for find user me")
     @ApiResponse(
@@ -70,6 +78,47 @@ public class UserController {
         UserInfoRespDto userInfoRespDto = userService.updateUserInfo(loginUser.getUser().getId(), username, description, imageUrl);
 
         return new ResponseEntity<>(new ResponseDto<>(1, "회원정보 수정 성공", userInfoRespDto), OK);
+    }
+
+    @Operation(summary = "social login", description = "api for social login")
+    @ApiResponse(
+            responseCode = "200",
+            description = "로그인 성공",
+            content = @Content(schema = @Schema(implementation = LoginResDto.LoginSuccessResDto.class))
+    )
+    @PostMapping("/login/{registrationId}")
+    public ResponseEntity login(@PathVariable("registrationId") String registrationId,
+                                @RequestBody LoginCode loginCode) {
+        // assign an oauthService corresponding to the registrationId
+        oAuthService = authServiceFactory.get(registrationId);
+        // get accessToken using code
+        String accessToken = oAuthService.getAccessToken(loginCode.getCode());
+        // get user information using accessToken
+        OAuthKakaoService.SocialUserInfo socialUserInfo = oAuthService.getUserInfo(accessToken);
+        // save or update oauth information
+        LoginResDto.LoginSuccessResDto loginSuccessResDto = oAuthService.saveOrUpdate(socialUserInfo);
+        return new ResponseEntity<>(new ResponseDto<>(1, "로그인 성공", loginSuccessResDto), OK);
+    }
+
+    @Operation(summary = "Canceling your account", description = "api for Canceling your account")
+    @ApiResponse(
+            responseCode = "200",
+            description = "계정 탈퇴 성공",
+            content = @Content(schema = @Schema(implementation = Void.class))
+    )
+    @DeleteMapping
+    public ResponseEntity withdraw(@AuthenticationPrincipal LoginUser loginUser,
+                                   @Valid @RequestBody(required = false) LoginCode loginCode) {
+        // find user information and extract registration
+        String registration = userService.findUserInfo(loginUser.getUser().getId()).getSocialId();
+
+        // allocate implemented oauth service
+        oAuthService = authServiceFactory.get(registration);
+
+        // delete user information
+        oAuthService.deleteUser(loginUser.getUser().getId(), (loginCode != null) ? loginCode.getCode() : null);
+
+        return new ResponseEntity<>(new ResponseDto<>(1, "계정 탈퇴 성공", null), OK);
     }
 
 }
